e548c2d62e3b733eadbc4fd6d20157a152d4dd6b
Fendly98 <f.belhomme.jouvet@gmail.com>
2022-03-17 18:10:06 +0100

    fixed

--- ClientApp/server.js
+++ ClientApp/server.js
@@ -2,10 +2,10 @@ const express = require("express");
 
 const app = express();
 
-app.use(express.static("./dist/angular-projet-annuel-staging"));
+app.use(express.static("./dist/client-app"));
 
 app.get("/*", function (req, res) {
-  res.sendFile("index.html", { root: "dist/angular-projet-annuel-staging" });
+  res.sendFile("index.html", { root: "dist/client-app" });
 });
 
 app.listen(process.env.PORT || 8080);
