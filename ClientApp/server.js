627eb8ba956016d3fda55e7cbaf578f0a8d26c0a
Fendly98 <f.belhomme.jouvet@gmail.com>
2022-03-17 17:22:36 +0100

    update

--- ClientApp/server.js
+++ ClientApp/server.js
@@ -8,6 +8,6 @@ app.get("/*", function (req, res) {
   res.sendFile("index.html", { root: "dist/angular-projet-annuel-staging" });
 });
 
-app.listen(process.env.PORT || 4200);
+app.listen(process.env.PORT || 8080);
 
-console.log(`Running on port ${process.env.PORT || 4200}`);
+console.log(`Running on port ${process.env.PORT || 8080}`);
